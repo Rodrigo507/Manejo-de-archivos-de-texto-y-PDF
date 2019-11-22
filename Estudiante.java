3f8e44721a021513f93079dbd96c09af1a0708f0
Rodrigo507 <d3signergfx@gmail.com>
2019-11-21 22:49:23 -0500

    Documentacion de la clase Estudiante

--- Estudiante.java
+++ Estudiante.java
@@ -1,14 +1,17 @@
 /**
  * Estudiante
  */
-public class Estudiante implements Comparable<Estudiante>{
+public class Estudiante implements Comparable<Estudiante>{//Implementamos la interface Comparable
+    /*Encapsulación de los datos(atributos) del objeto*/
     private final String cedula;
     private final String apellido;
     private final String nombre;
     private final char sexo;
     private final String turno;
     private final int anno;
-    Estudiante (final String ced,final String ape, final String nom,final char se, final String tur, final int an){
+    /********************************************/
+    //Contrutor del objeto
+    Estudiante (String ced,String ape,String nom,char se,String tur,int an){
         this.cedula=ced;
         this.apellido=ape;
         this.nombre=nom;
@@ -16,36 +19,41 @@ public class Estudiante implements Comparable<Estudiante>{
         this.turno=tur;
         this.anno=an;
     }
-    
-	/*METODOS */
+    /********************************************/
+    /*METODOS */
+    /**
+     * Con estos metodos
+     * devolvemos los atributos del objeto
+     */
     public String getcedula() {
         return(this.cedula);
-}
-public String getApellido() {
-            return(this.apellido);
-}
-public String getNombre() {
-            return(this.nombre);
-}
-public char getSexo() {
-            return(this.sexo);
-}
-public String getTurno() {
-            return(this.turno);
-}
-public int getAnno() {
-    return(this.anno);
-}
-
-@Override
-     public int compareTo(final Estudiante o) {
+    }
+    public String getApellido() {
+        return(this.apellido);
+    }
+    public String getNombre() {
+        return(this.nombre);
+    }
+    public char getSexo() {
+        return(this.sexo);
+    }
+    public String getTurno() {
+        return(this.turno);
+    }
+    public int getAnno() {
+        return(this.anno);
+    }
+    /********************************************/
+    /*Metodo comparable*/
+    /*Con este metodo realizamos la ordenacion del ArrayList*/
+    @Override
+    public int compareTo(final Estudiante o) {
         if (this.sexo < o.sexo) {
             return -1;
-        }
-        if (this.sexo == o.sexo) {
+        }if (this.sexo == o.sexo) {
             return 0;
         }
         return 1;
-     }
+    }    
    
 }
