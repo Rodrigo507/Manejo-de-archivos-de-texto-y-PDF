18afa125073e9cbc101601ec3cf8113a38898143
Rodrigo507 <d3signergfx@gmail.com>
2019-11-22 01:23:09 -0500

    Agreo imagen y un mensaje con la carrera selecionada

--- Informe.java
+++ Informe.java
@@ -1,10 +1,13 @@
 /*IMPORTACION DE DE itextpdf */
 
+import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+
 /**
  * Informe
  */
@@ -14,14 +17,30 @@ import java.util.ArrayList;
 public class Informe {
 
 
-    public void pdfinforme(ArrayList<Estudiante> carrera) {
-        
+    public void pdfinforme(ArrayList<Estudiante> carrera, String carreraselected) {
+    
+
     
     Document archivo = new Document();
     try {
         PdfWriter.getInstance(archivo, new FileOutputStream("INFORME PDF"));//Instanciamos el archivo
         archivo.open();//Abrimos el archivo
-        PdfPTable primera = new PdfPTable(4);
+        //Insertar imagen
+
+        try
+        {
+            Image foto = Image.getInstance("bannerWebCruvFiec2.png");
+            foto.scaleToFit(500, 600);
+            foto.setAlignment(Chunk.ALIGN_MIDDLE);
+            archivo.add(foto);
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+        }
+
+        archivo.add(new Paragraph("\n\nCarrera: "+carreraselected+"\n\n"));
+        PdfPTable primera = new PdfPTable(4);//Generamos la columnas para la primera fila de la tabla
         primera.addCell("NOMBRE");//Agregamos a cada celda lo que deseamos
         primera.addCell("APELLIDO");//Agregamos a cada celda lo que deseamos
         primera.addCell("CEDULA");//Agregamos a cada celda lo que deseamos
@@ -37,7 +56,7 @@ public class Informe {
         }
         archivo.add(tabla);//Agregamos la tabla al archivo
 
-        archivo.add(new Paragraph("A Hello World PDF document."));
+        
         archivo.close(); // Serramos el documento
     } catch (Exception e) {
         e.printStackTrace();
