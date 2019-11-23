09576e9268c0c8b1c12a4dc93702b8db9bcfd6b6
Rodrigo507 <d3signergfx@gmail.com>
2019-11-23 14:07:34 -0500

    Se cambio la forma de crear el nuevo informe con una nueva variable

--- Informe.java
+++ Informe.java
@@ -4,6 +4,7 @@ import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
@@ -17,19 +18,27 @@ import java.util.ArrayList;
 public class Informe {
 
 
-    public void pdfinforme(ArrayList<Estudiante> carrera, String carreraselected) {
-    
+    public void pdfinforme(ArrayList<Estudiante> carrera, String carreraselected, String img) {
 
-    
-    Document archivo = new Document();
+
+
+    //Document archivo = new Document();
     try {
-        PdfWriter.getInstance(archivo, new FileOutputStream("INFORME PDF"));//Instanciamos el archivo
-        archivo.open();//Abrimos el archivo
+        //PdfWriter.getInstance(archivo, new FileOutputStream("INFORME PDF"));//Instanciamos el archivo
+        FileOutputStream dc = new FileOutputStream("Pie de pagina");
+        Document archivo = new Document(PageSize.A4, 20, 50, 50, 50);
+        //Document archivos = new Document(pageSize, marginLeft, marginRight, marginTop, marginBottom);
+        PdfWriter writer =PdfWriter.getInstance(archivo, dc);
         //Insertar imagen
+        /*Header and Footer*/
+        writer.setInitialLeading(16);
 
+        HeaderFooter top = new HeaderFooter(carreraselected);//Mensaje del header = carrera que seleciono el usuario
+        writer.setPageEvent(top);
+        archivo.open();//Abrimos el archivo
         try
         {
-            Image foto = Image.getInstance("bannerWebCruvFiec2.png");
+            Image foto = Image.getInstance(img);//Logo 
             foto.scaleToFit(500, 600);
             foto.setAlignment(Chunk.ALIGN_MIDDLE);
             archivo.add(foto);
@@ -48,7 +57,7 @@ public class Informe {
         archivo.add(primera);
         PdfPTable tabla = new PdfPTable(4);//Generamos la columnas
         for (Estudiante z : carrera) {
-           
+
             tabla.addCell(z.getNombre());
             tabla.addCell(z.getApellido());
             tabla.addCell(z.getcedula());
@@ -56,12 +65,12 @@ public class Informe {
         }
         archivo.add(tabla);//Agregamos la tabla al archivo
 
-        
+
         archivo.close(); // Serramos el documento
     } catch (Exception e) {
         e.printStackTrace();
     }
 
     }
-    
+
 }
