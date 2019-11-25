6ce94a7eee4602e449c3021d87ac7de9ed95d9d5
Rodrigo507 <d3signergfx@gmail.com>
2019-11-25 01:13:02 -0500

    Se agrego un metodo PORCENTAJE

--- Principal.java
+++ Principal.java
@@ -7,9 +7,9 @@ import java.util.Collections;
  * Principal
  */
 public class Principal {
-    public static void main(String[] args) {
-        InteractiveIO mensajes = new InteractiveIO();
-        Informe crear = new Informe();
+    public static void main(final String[] args) {
+        final InteractiveIO mensajes = new InteractiveIO();
+        final Informe crear = new Informe();
         /************************************/
         String linea;
         int op=0;
@@ -21,17 +21,19 @@ public class Principal {
         String turno;
         int anno;
         /***********************************/
-        ArrayList<Estudiante> ingeneria=new ArrayList<Estudiante>();
-        ArrayList<Estudiante> tecnico=new ArrayList<Estudiante>();
-        ArrayList<Estudiante> licenciatura=new ArrayList<Estudiante>();
+        int masculino=0;
+        int femenino=0;
+        /***********************************/
+        final ArrayList<Estudiante> ingeneria=new ArrayList<Estudiante>();
+        final ArrayList<Estudiante> tecnico=new ArrayList<Estudiante>();
+        final ArrayList<Estudiante> licenciatura=new ArrayList<Estudiante>();
         try {
             // Abrimos el archivo del fichero y creamos un BufferedReader para poder
             // realizar la lectura del mismo
-            FileReader archivo= new FileReader("Directorio_Estudiantes.txt");
-            BufferedReader lectura=new BufferedReader(archivo);
+            final FileReader archivo= new FileReader("Directorio_Estudiantes.txt");
+            final BufferedReader lectura=new BufferedReader(archivo);
             linea=lectura.readLine();
         while (linea!=null) {
-            //if (linea.contains("Carrera")) {//Si en la linea encuentra la secuencia "Carrera"
                 if (linea.contains("LIC. DE INGENIERIA EN INFORMATICA")) {//Verificamos la carrera que vamos a acceder
                     op=1;
                 }else if (linea.contains("TECNICO EN INFORMATICA EDUCATIVA")) {//Verificamos la carrera que vamos a acceder
@@ -39,8 +41,8 @@ public class Principal {
                 }else if (linea.contains("LIC. PARA LA GESTION EDUC Y EMPRESA")) {//Verificamos la carrera que vamos a acceder
                     op=3;
                 }
-           // }
-            if (op==1) {
+
+            if (op==1) {//Solo entrara cuando lineas antes leemos la primera carrera
                 if (linea.startsWith("0")||linea.startsWith("1")){
                     cedula=((linea.substring(0,16)).trim()).replace(" ","-");
                     apellido=(linea.substring(17,36)).trim();
@@ -76,33 +78,57 @@ public class Principal {
             linea=lectura.readLine();
         }
         lectura.close();
-        } catch (Exception e) {//En caso de error a la hora de abrir el archivo .txt
+        } catch ( Exception e) {//En caso de error a la hora de abrir el archivo .txt
             mensajes.desplegarError("Error en la lectura del archivo\n\n"+e, "ERRO EN LA LECTURA");
             System.exit(1);//EL PROGRAMA SE TERMINA
             //TODO: handle exception
         }
 
-        String carreraselect=mensajes.getCarreta();
+         String carreraselect=mensajes.getCarreta();
         /*Realizamos la ordenacion de los arrayList por sexo*/
         /*La ordenacion se realiza con la interface COMPARABLE */
         Collections.sort(ingeneria);
         Collections.sort(tecnico);
         Collections.sort(licenciatura);
         if (carreraselect.equals("LIC. DE INGENIERIA EN INFORMATICA")) {
+            crear.pdfinforme(ingeneria,carreraselect,"imagenes/lii.png");
             for (Estudiante estudiante : ingeneria) {
-                crear.pdfinforme(ingeneria,carreraselect,"lii.png");
-                mensajes.desplegarINFORMA("SE A GENERADO EL INFORME CORRECTAMENTE","INFORME");
+                if (estudiante.getSexo().equals("M")) {
+                    masculino++;
+                }else{
+                    femenino++;
+                }
             }
+            mensajes.desplegarINFORMA("SE A GENERADO EL INFORME CORRECTAMENTE","INFORME");
         }else if (carreraselect.equals("TECNICO EN INFORMATICA EDUCATIVA")) {
+            crear.pdfinforme(tecnico,carreraselect,"imagenes/tecnico.png");
             for (Estudiante estudiante : tecnico) {
-                crear.pdfinforme(tecnico,carreraselect,"tecnico.png");
-               mensajes.desplegarINFORMA("SE A GENERADO EL INFORME CORRECTAMENTE","INFORME");
+                if (estudiante.getSexo().equals("M")) {
+                    masculino++;
+                }else{
+                    femenino++;
+                }
             }
+            mensajes.desplegarINFORMA("SE A GENERADO EL INFORME CORRECTAMENTE","INFORME");
         }else if(carreraselect.equals("LIC. PARA LA GESTION EDUC Y EMPRESA")) {
+            crear.pdfinforme(licenciatura,carreraselect,"imagenes/ligee.png");
             for (Estudiante estudiante : licenciatura) {
-                crear.pdfinforme(licenciatura,carreraselect,"ligee.png");
-                mensajes.desplegarINFORMA("SE A GENERADO EL INFORME CORRECTAMENTE","INFORME");
+                if (estudiante.getSexo().equals("M")) {
+                    masculino++;
+                }else{
+                    femenino++;
+                }
             }
+            mensajes.desplegarINFORMA("SE A GENERADO EL INFORME CORRECTAMENTE","INFORME");
         }
+        mensajes.desplegarINFORMA("MASCULINOS: "+ porncentaje(masculino,femenino)+"%"+"\nFEMENINAS: "+porncentaje(femenino, masculino)+"%"+"\n Total: "+(masculino+femenino), "Porcentaje Por Sexo");
+
+        System.exit(0);
     }
+    //Metododos clase principal
+    public static double porncentaje(int num1,int num2) {
+        return((100*num1)/(num1+num2));
+    }
+
+
 }
