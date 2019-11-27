b416990fa1003c19b75e0ead455adee8c7bd0ee0
Rodrigo507 <d3signergfx@gmail.com>
2019-11-27 00:58:04 -0500

    Documentacion de la clase Estudiante

--- Estudiante.java
+++ Estudiante.java
@@ -1,7 +1,7 @@
-
-
 /**
  * Estudiante
+ * Esta clase nos permite la creacion de objetos
+ * ademas que contiene metodos para devolver los datos
  */
 public class Estudiante implements Comparable<Estudiante>{//Implementamos la interface Comparable
     /*Encapsulación de los datos(atributos) del objeto*/
@@ -12,7 +12,7 @@ public class Estudiante implements Comparable<Estudiante>{//Implementamos la int
     private  String turno;
     private  int anno;
     /********************************************/
-    //Contrutor del objeto
+    //Construtor del objeto
     Estudiante (String ced,String ape,String nom,String se,String tur,int an){
         this.cedula=ced;
         this.apellido=ape;
@@ -48,6 +48,7 @@ public class Estudiante implements Comparable<Estudiante>{//Implementamos la int
     /********************************************/
     /*Metodo comparable*/
     /*Con este metodo realizamos la ordenacion del ArrayList*/
+    /*Por sexo */
     @Override
     public int compareTo( Estudiante o) {
         return(this.sexo.compareTo(o.sexo));
